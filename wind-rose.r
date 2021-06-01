7a47d96e0042b829345f588270575f1340b28c9d
David Kahler <dmkahler@users.noreply.github.com>
2021-05-31 21:02:14 -0400

    added site.  next, add speed visualization.

--- wind-rose.r
+++ wind-rose.r
@@ -3,10 +3,18 @@
 
 library(tidyverse) # includes ggplot and readr commands
 
-x <- read_csv("/Users/davidkahler/Documents/Air_Quality/PGH_weather.csv")
-# wind direction data are at x$`Wind Dir`
-br <- 10*(c(0:36))
+# Pittsburgh International Airport (PIT)
+x <- read_csv("https://duq.box.com/shared/static/i9qlh63qdzf5hqvf40dphkbwkh93o2h0.csv")
+
+# PNC Park (PNC)
+x <- read_csv("https://duq.box.com/shared/static/2cs6xi81xtcmq4mmi46t0v0ev4f2mehs.csv")
+
+# wind direction data are at:
+# PIT: x$`Wind Dir`
+# PNC: x$`Wind Vane`
+br <- 10*(c(0:36)) # This array constructs the bins in degrees
 h <- hist(x$`Wind Dir`, breaks = br)
+h <- hist(x$`Wind Vane`, breaks = br)
 
 # Make rose plot, based on:
 # https://stackoverflow.com/questions/39024758/how-to-use-r-package-circular-to-make-rose-plot-of-histogram-data-on-360/39025913
@@ -14,23 +22,14 @@ h <- hist(x$`Wind Dir`, breaks = br)
 angle <- h$mids
 count <- h$counts
 y <- data.frame(angle, count)
-ggplot(y, aes(x = angle, y = count)) +
+ggplot(y, aes(x = angle, y = count)) + 
+      labs(caption = "Pittsburgh International Airport") + 
       geom_col(fill = "steelblue", color = "steelblue") +
       coord_polar(theta = "x", start = 0) +
       scale_x_continuous(breaks = seq(0, 360, 45)) +
       theme_linedraw() +
-      theme(axis.title = element_blank(), panel.ontop = TRUE, panel.background = element_blank())
+      theme(axis.title = element_blank(), panel.ontop = TRUE, panel.background = element_blank()) 
 
-# April only
-a <- x[which(x$Month == 4), names(x) %in% c("Year", "Month", "Day", "Atemp", "RH", "Wind Dir", "Precip")]
-g <- hist(a$`Wind Dir`, breaks = br)
-ga <- g$mids
-gc <- g$counts
-b <- data.frame(ga, gc)
-ggplot(b, aes(x = ga, y = gc)) +
-      geom_col(fill = "steelblue", color = "steelblue") +
-      coord_polar(theta = "x", start = 0) +
-      scale_x_continuous(breaks = seq(0, 360, 45)) +
-      theme_linedraw() +
-      theme(axis.title = element_blank(), panel.ontop = TRUE, panel.background = element_blank())
 
+# caption = "Pittsburgh International Airport" + 
+# caption = "PNC Park" + 
