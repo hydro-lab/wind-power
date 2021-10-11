e91910fbd268637a20448f56898215509b838471
mkranii <85361544+mkranii@users.noreply.github.com>
2021-10-11 15:42:15 -0400

    10112021

--- datapull.R
+++ datapull.R
@@ -11,13 +11,105 @@ library(lubridate)
 cath <- read_csv("https://duq.box.com/shared/static/9dryp6f7zdqxnd5hevx01d2j5sxuj0yt.csv")
 cath <- cath %>%
       rename(wspd=`Anemometer (MPH)`) %>%
+      rename(gust=`10 Minute Wind Gust (MPH)`) %>%
+      rename(baro=`Barometer (Inches of Hg)`) %>%
+      rename(rhum=`Hygrometer (% Humidity)`) %>%
+      rename(prcp=`Rain Gauge (Inches)`) %>%
+      rename(srrd=`Solar Radiation Sensor (Watts Per Square Meter)`) %>%
+      rename(temp=`Thermometer (Fahrenheit)`) %>%
+      rename(wdir=`Wind Vane (Degrees)`) %>%
       mutate(dt = mdy_hm(cath$Timestamp, tz="US/Eastern")) # Stores time with time zone in lubridate function (POSIXct)
 cath$wspd <- cath$wspd * 0.44704
+cath$gust <- cath$gust * 0.44704
+cath$baro <- cath$baro * 25.4
+cath$prcp <- cath$prcp * 25.4
+cath$temp <- cath$temp - 32 * 0.5556
 
 # Heinz Field
 henz <- read_csv("https://duq.box.com/shared/static/hg7mr85uqnqwoqlrgcd37x3ce7n8qpdm.csv")
 henz <- henz %>%
       rename(wspd=`Anemometer (MPH)`) %>%
+      rename(gust=`10 Minute Wind Gust (MPH)`) %>%
+      rename(baro=`Barometer (Inches of Hg)`) %>%
+      rename(rhum=`Hygrometer (% Humidity)`) %>%
+      rename(prcp=`Rain Gauge (Inches)`) %>%
+      rename(srrd=`Solar Radiation Sensor (Watts Per Square Meter)`) %>%
+      rename(temp=`Thermometer (Fahrenheit)`) %>%
+      rename(wdir=`Wind Vane  (Degrees)`) %>%
       mutate(dt = mdy_hm(henz$Timestamp, tz="US/Eastern"))
+henz$wspd <- henz$wspd * 0.44704
+henz$gust <- henz$gust * 0.44704
+henz$baro <- henz$baro * 25.4
+henz$prcp <- henz$prcp * 25.4
+henz$temp <- henz$temp - 32 * 0.5556
+
+#Falk School
+falk <- read_csv("https://duq.box.com/shared/static/uhwwur6mv9kipo6tdxa7h265x5ss1x36.csv")
+falk <- falk %>%
+      rename(wspd=`Anemometer (MPH)`) %>%
+      rename(gust=`10 Minute Wind Gust (MPH)`) %>%
+      rename(baro=`Barometer (Inches of Hg)`) %>%
+      rename(rhum=`Hygrometer (% Humidity)`) %>%
+      rename(prcp=`Rain Gauge (Inches)`) %>%
+      rename(srrd=`Solar Radiation Sensor (Watts per Square Meter)`) %>%
+      rename(temp=`Thermometer (Fahrenheit)`) %>%
+      rename(wdir=`Wind Vane (Degrees)`) %>%
+      mutate(dt = mdy_hm(Timestamp, tz="US/Eastern"))
+falk$wspd <- falk$wspd * 0.44704
+falk$gust <- falk$gust * 0.44704
+falk$baro <- falk$baro * 25.4
+falk$prcp <- falk$prcp * 25.4
+falk$temp <- falk$temp - 32 * 0.5556
 
 
+#Environmental Charter School
+char <- read_csv("https://duq.box.com/shared/static/q2wptl6izaeu1f4piz9gglxk0k50qfwr.csv")
+char <- char %>%
+  rename(wspd=`Anemometer (Miles Per Hour)`) %>%
+  rename(gust=`10 Minute Wind Gust (Miles Per Hour)`) %>%
+  rename(baro=`Barometer (Inches of Hg)`) %>%
+  rename(rhum=`Hygrometer (% Humidity)`) %>%
+  rename(prcp=`Rain Gauge (Inches)`) %>%
+  rename(srrd=`Solar Radiation Sensor (Watts Per Square Meter)`) %>%
+  rename(temp=`Thermometer (Fahrenheit)`) %>%
+  rename(wdir=`Wind Vane (Degrees)`) %>%
+  mutate(dt = mdy_hm(Timestamp, tz="US/Eastern"))
+char$wspd <- char$wspd * 0.44704
+char$gust <- char$gust * 0.44704
+char$baro <- char$baro * 25.4
+char$prcp <- char$prcp * 25.4
+char$temp <- char$temp - 32 * 0.5556
+
+
+#Penn State
+penn <- read_csv("https://duq.box.com/shared/static/m88w4uhb5nx5uctq7t4wfmzd3lq8v20r.csv")
+penn <- penn %>%
+  rename(wspd=`Anemometer (MPH)`) %>%
+  rename(gust=`10 Minute Wind Gust (MPH)`) %>%
+  rename(baro=`Barometer (Inches of Hg)`) %>%
+  rename(rhum=`Hygrometer (% Humidity)`) %>%
+  rename(prcp=`Rain Gauge (Inches)`) %>%
+  rename(srrd=`Solar Radiation Sensor (Watts Per Square Meter)`) %>%
+  rename(temp=`Thermometer (Fahrenheit)`) %>%
+  rename(wdir=`Wind Vane (Degrees)`) %>%
+  mutate(dt = mdy_hm(Timestamp, tz="US/Eastern"))
+penn$wspd <- penn$wspd * 0.44704
+penn$gust <- penn$gust * 0.44704
+penn$baro <- penn$baro * 25.4
+penn$prcp <- penn$prcp * 25.4
+penn$temp <- penn$temp - 32 * 0.5556
+
+
+#Pittsburgh International Airport
+#pita <- read_csv("https://duq.box.com/shared/static/1rdzbgmp6kti0prpturzzzym7k161c37.csv")
+pita <- read_csv("https://duq.box.com/shared/static/g2h3c34irmotlh2n1wz4dttjs8tgonwl.csv", col_types = "cnnnnnnnnnnnccccc")
+pita <- pita%>%
+  rename(wspd=`Average Wind Speed (mph)`) %>%
+  rename(gust=`Max Wind Gust (mph)`) %>%
+  rename(rhum=`Average Relative Humidity (%)`) %>%
+  rename(prcp=`1 Hour Precip (in)`) %>%
+  rename(temp=`Average Temp (F)`) %>%
+  rename(wdir=`Average Wind Direction (deg)`) %>%
+  rename(baro=`Average Station Pressure (mb)`) %>%
+  mutate(dt = mdy_hm(`Date/Time (GMT)`, tz="GMT"))%>%
+  select(dt, wspd, gust, rhum, prcp, temp, wdir, baro)
