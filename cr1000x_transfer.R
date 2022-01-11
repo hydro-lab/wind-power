d0979fe4dc6d45e3a304564ecc74e29d72dddf69
David Kahler <david.m.kahler@gmail.com>
2022-01-11 14:20:17 -0500

    Revised the level 1 data output.

--- cr1000x_transfer.R
+++ cr1000x_transfer.R
@@ -16,7 +16,7 @@ install <- as.numeric(ymd_hms("2021-11-12T20:30:00")) # this is time in UTC
 # previous download got to:
 first_record <- t$X3[nrow(t)] + 1 # moves to one second after last download, must rewrite
 # sort and tidy data. 
-x <- x %>% 
+cr1000x <- x %>% 
      rename(TIMESTAMP = X1, # date and time
             RECORD = X2, # sequential record number
             BattV_Avg = X3, # battery voltage, average of 15 minute
@@ -47,7 +47,8 @@ x <- x %>%
             utc_offset = (as.numeric(force_tz(time_et, tz = "UTC")) - unix_utc)/3600 ) %>%
      select(-TIMESTAMP)
 
-#filter(unix_utc >= first_record) # This is done to only take the data since the last download.
+x <- cr1000x %>% 
+     filter(unix_utc >= first_record) # This is done to only take the data since the last download.
 
 first_record <- x$unix_utc[1] # use this in subsequent downloads, not if the post-install must be redone.
 
@@ -127,24 +128,21 @@ level0 <- level0 %>%
 write_csv(level0, "/Users/davidkahler/Documents/Wind_Turbines/DuqMellon_HIS.csv", append = TRUE)
 
 # QA/QC checks:
-
-BattV_Avg_qc = "", 
-AirTC_Avg_qc = "", 
-AirTC_Max_qc = "", 
-AirTC_Min_qc = "", 
-AirTC_Std_qc = "", 
-#RHpct_Min = NA,
-RHpct_Min_qc = "", 
-#RHpct_Max = NA,
-RHpct_Max_qc = "", 
-WS_ms_Avg_qc = "", 
-WS_ms_Max_qc = "", 
-WS_ms_Min_qc = "", 
-WS_ms_Std_qc = "", 
-WindDir_D1_WVT_qc = "", 
-WindDir_SD1_WVT_qc = "", 
-Rain_mm_Tot_qc = "") %>% 
-     
+x <- x %>% # allocate blank qc flags
+     mutate(BattV_Avg_qc = "", 
+            AirTC_Avg_qc = "", 
+            AirTC_Max_qc = "", 
+            AirTC_Min_qc = "", 
+            AirTC_Std_qc = "", 
+            RHpct_Min_qc = "", 
+            RHpct_Max_qc = "", 
+            WS_ms_Avg_qc = "", 
+            WS_ms_Max_qc = "", 
+            WS_ms_Min_qc = "", 
+            WS_ms_Std_qc = "", 
+            WindDir_D1_WVT_qc = "", 
+            WindDir_SD1_WVT_qc = "", 
+            Rain_mm_Tot_qc = "")
 
 for (i in 1:nrow(x)) {
      if (x$BattV_Avg[i] >= 10) {
@@ -170,17 +168,18 @@ for (i in 1:nrow(x)) {
           x$AirTC_Avg_qc[i] <- paste0(x$AirTC_Avg_qc[i], "m,") #
      } else {
           if (x$AirTC_Avg[i] < -40) {
+               x$AirTC_Avg[i] <- -9999
                x$AirTC_Avg_qc[i] <- paste0(x$AirTC_Avg_qc[i], "l,") # "l" temperature below valid range
           } else if (x$AirTC_Avg[i] > 70) {
+               x$AirTC_Avg[i] <- -9999
                x$AirTC_Avg_qc[i] <- paste0(x$AirTC_Avg_qc[i], "h,") # "h" temperature above valid range
           }
-          if ((x$AirTC_Avg[i] <= x$AirTC_Max[i]) & (x$AirTC_Avg[i] >= x$AirTC_Min[i])) { # If average is outside the range [min,max]
-               x$AirTC_Avg_qc[i] <- x$AirTC_Avg_qc[i]
-          } else {
+          if ((x$AirTC_Avg[i] > x$AirTC_Max[i]) | (x$AirTC_Avg[i] < x$AirTC_Min[i])) { # If average is outside the range [min,max]
+               x$AirTC_Avg[i] <- -9999
                x$AirTC_Avg_qc[i] <- paste0(x$AirTC_Avg_qc[i], "i,") # "i" instrument error
           }
           if (i > 4) {
-               if ( (x$AirTC_Avg[i] < (mean(x$AirTC_Avg[(i-4):(i-1)]) - 3 * ( max(x$AirTC_Avg[(i-4):(i-1)]) - min(x$AirTC_Avg[(i-4):(i-1)]) ) )) | (x$AirTC_Avg[i] > (mean(x$AirTC_Avg[(i-4):(i-1)]) + 3 * ( max(x$AirTC_Avg[(i-4):(i-1)]) - min(x$AirTC_Avg[(i-4):(i-1)]) ) )) ) {
+               if ( (x$AirTC_Avg[i] < (mean(x$AirTC_Avg[(i-4):(i-1)]) - 2 * ( mean(x$AirTC_Max[(i-4):(i-1)]) - mean(x$AirTC_Min[(i-4):(i-1)]) ) )) | (x$AirTC_Avg[i] > (mean(x$AirTC_Avg[(i-4):(i-1)]) + 2 * ( mean(x$AirTC_Max[(i-4):(i-1)]) - mean(x$AirTC_Min[(i-4):(i-1)]) ) )) ) {
                     x$AirTC_Avg_qc[i] <- paste0(x$AirTC_Avg_qc[i], "s,") # "s" spike detected
                }
           }
@@ -190,8 +189,10 @@ for (i in 1:nrow(x)) {
           x$AirTC_Max_qc[i] <- paste0(x$AirTC_Max_qc[i], "m,") #
      } else {
           if (x$AirTC_Max[i] < -40) {
+               x$AirTC_Max[i] <- -9999
                x$AirTC_Max_qc[i] <- paste0(x$AirTC_Max_qc[i], "l,")
           } else if (x$AirTC_Max[i] > 70) {
+               x$AirTC_Max[i] <- -9999
                x$AirTC_Max_qc[i] <- paste0(x$AirTC_Max_qc[i], "h,")
           }
      }
@@ -200,8 +201,10 @@ for (i in 1:nrow(x)) {
           x$AirTC_Min_qc[i] <- paste0(x$AirTC_Min_qc[i], "m,") #
      } else {
           if (x$AirTC_Min[i] < -40) {
+               x$AirTC_Min[i] <- -9999
                x$AirTC_Min_qc[i] <- paste0(x$AirTC_Min_qc[i], "l,")
           } else if (x$AirTC_Min[i] > 70) {
+               x$AirTC_Min[i] <- -9999
                x$AirTC_Min_qc[i] <- paste0(x$AirTC_Min_qc[i], "h,")
           }
      }
@@ -214,15 +217,12 @@ for (i in 1:nrow(x)) {
           x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "m,") #
      } else {
           if (x$RHpct_Min[i] < 0) {
+               if (x$RHpct_Min[i] < -1) {x$RHpct_Min[i] <- -9999}
                x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "l,")
           }
           if (x$RHpct_Min[i] > 100) {
+               if (x$RHpct_Min[i] > 101) {x$RHpct_Min[i] <- -9999}
                x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "h,")
-          }
-          if (i > 4) {
-               if ( (x$RHpct_Min[i] < (mean(x$RHpct_Min[(i-4):(i-1)]) - 3 * ( max(x$RHpct_Min[(i-4):(i-1)]) - min(x$RHpct_Min[(i-4):(i-1)]) ) )) | (x$RHpct_Min[i] > (mean(x$RHpct_Min[(i-4):(i-1)]) + 3 * ( max(x$RHpct_Min[(i-4):(i-1)]) - min(x$RHpct_Min[(i-4):(i-1)]) ) )) ) {
-                    x$RHpct_Min_qc[i] <- paste0(x$RHpct_Min_qc[i], "s,") # "s" spike detected
-               }
           }
      }
      if (is.na(x$RHpct_Max[i])) {
@@ -230,15 +230,12 @@ for (i in 1:nrow(x)) {
           x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "m,") #
      } else {
           if (x$RHpct_Max[i] < 0) {
+               if (x$RHpct_Max[i] < -1) {x$RHpct_Max[i] <- -9999}
                x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "l,")
           }
           if (x$RHpct_Max[i] > 100) {
+               if (x$RHpct_Max[i] > 101) {x$RHpct_Max[i] <- -9999}
                x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "h,")
-          }
-          if (i > 4) {
-               if ( (x$RHpct_Max[i] < (mean(x$RHpct_Max[(i-4):(i-1)]) - 3 * ( max(x$RHpct_Max[(i-4):(i-1)]) - min(x$RHpct_Max[(i-4):(i-1)]) ) )) | (x$RHpct_Max[i] > (mean(x$RHpct_Max[(i-4):(i-1)]) + 3 * ( max(x$RHpct_Max[(i-4):(i-1)]) - min(x$RHpct_Max[(i-4):(i-1)]) ) )) ) {
-                    x$RHpct_Max_qc[i] <- paste0(x$RHpct_Max_qc[i], "s,") # "s" spike detected
-               }
           }
      }
      if (is.na(x$WS_ms_Avg[i])) {
@@ -246,9 +243,11 @@ for (i in 1:nrow(x)) {
           x$WS_ms_Avg_qc[i] <- paste0(x$WS_ms_Avg_qc[i], "m,") #
      } else {
           if (x$WS_ms_Avg[i] < 0) {
+               x$WS_ms_Avg[i] <- -9999
                x$WS_ms_Avg_qc[i] <- paste0(x$WS_ms_Avg_qc[i], "l,")
           }
           if (x$WS_ms_Avg[i] > 100) {
+               x$WS_ms_Avg[i] <- -9999
                x$WS_ms_Avg_qc[i] <- paste0(x$WS_ms_Avg_qc[i], "h,")
           }
      }
@@ -257,9 +256,11 @@ for (i in 1:nrow(x)) {
           x$WS_ms_Min_qc[i] <- paste0(x$WS_ms_Min_qc[i], "m,") #
      } else {
           if (x$WS_ms_Min[i] < 0) {
+               x$WS_ms_Min[i] <- -9999
                x$WS_ms_Min_qc[i] <- paste0(x$WS_ms_Min_qc[i], "l,")
           }
           if (x$WS_ms_Min[i] > 100) {
+               x$WS_ms_Min[i] <- -9999
                x$WS_ms_Min_qc[i] <- paste0(x$WS_ms_Min_qc[i], "h,")
           }
      }
@@ -268,9 +269,11 @@ for (i in 1:nrow(x)) {
           x$WS_ms_Max_qc[i] <- paste0(x$WS_ms_Max_qc[i], "m,") #
      } else {
           if (x$WS_ms_Max[i] < 0) {
+               x$WS_ms_Max[i] <- -9999
                x$WS_ms_Max_qc[i] <- paste0(x$WS_ms_Max_qc[i], "l,")
           }
           if (x$WS_ms_Min[i] > 100) {
+               x$WS_ms_Max[i] <- -9999
                x$WS_ms_Max_qc[i] <- paste0(x$WS_ms_Max_qc[i], "h,")
           }
      }
@@ -279,9 +282,11 @@ for (i in 1:nrow(x)) {
           x$WindDir_D1_WVT_qc[i] <- paste0(x$WindDir_D1_WVT_qc[i], "m,") #
      } else {
           if (x$WindDir_D1_WVT[i] < 0) {
+               x$WindDir_D1_WVT[i] <- -9999
                x$x$WindDir_D1_WVT_qc[i] <- paste0(x$WindDir_D1_WVT_qc[i], "l,")
           }
           if (x$WindDir_D1_WVT[i] > 360) {
+               x$WindDir_D1_WVT[i] <- -9999
                x$WindDir_D1_WVT_qc[i] <- paste0(x$WindDir_D1_WVT_qc[i], "h,")
           }
      }
@@ -303,10 +308,7 @@ for (i in 1:nrow(x)) {
 
 #y <- x[,c(13,14,15,16,1,17,2,18,4,20,3,19,5,21,22,23,24,25,6,26,8,28,7,27,9,29,10,30,11,31,12,32)] # old, w/o RH
 y <- x[,c(15,16,17,18,1,19,2,20,4,22,3,21,5,23,14,24,13,25,6,26,8,28,7,27,9,29,10,30,11,31,12,32)] # New, post - 01 Dec 2021
-#write_csv(y, "/Users/davidkahler/Documents/Wind_Turbines/mellon_table.csv", append = TRUE) # commenting since we're mainly going to CUAHSI
-
-# Add quality control levels
-
+write_csv(y, "/Users/davidkahler/Documents/Wind_Turbines/mellon_table.csv", append = TRUE) 
 
 # Long format for CUAHSI upload
 z <- pivot_longer(x, cols = c(BattV_Avg,AirTC_Avg,AirTC_Min,AirTC_Max,AirTC_Std,RHpct_Min,RHpct_Max,WS_ms_Avg,WS_ms_Min,WS_ms_Max,WS_ms_Std,WindDir_D1_WVT,WindDir_SD1_WVT,Rain_mm_Tot),
@@ -314,87 +316,74 @@ z <- pivot_longer(x, cols = c(BattV_Avg,AirTC_Avg,AirTC_Min,AirTC_Max,AirTC_Std,
                   values_to = "Value")
 z$site <- "DuqMellon"
 z$source <- "Duq-CERE"
+z$qc <- "Level1"
 
 batt <- z %>% 
      filter(Variable=="BattV_Avg") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,BattV_Avg_qc) %>% 
-     rename(qc = BattV_Avg_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Voltmeter")
 air_avg <- z %>% 
      filter(Variable=="AirTC_Avg") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,AirTC_Avg_qc) %>% 
-     rename(qc = AirTC_Avg_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 air_min <- z %>% 
      filter(Variable=="AirTC_Min") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,AirTC_Min_qc) %>% 
-     rename(qc = AirTC_Min_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 air_max <- z %>% 
      filter(Variable=="AirTC_Max") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,AirTC_Max_qc) %>% 
-     rename(qc = AirTC_Max_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 air_std <- z %>% 
      filter(Variable=="AirTC_Std") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,AirTC_Std_qc) %>% 
-     rename(qc = AirTC_Std_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 rh_min <- z %>% 
      filter(Variable=="RHpct_Min") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,RHpct_Min_qc) %>% 
-     rename(qc = RHpct_Min_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 rh_max <- z %>% 
      filter(Variable=="RHpct_Max") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,RHpct_Max_qc) %>% 
-     rename(qc = RHpct_Max_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Thermometer_hygrometer")
 ws_avg <- z %>% 
      filter(Variable=="WS_ms_Avg") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WS_ms_Avg_qc) %>% 
-     rename(qc = WS_ms_Avg_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 ws_min <- z %>% 
      filter(Variable=="WS_ms_Min") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WS_ms_Min_qc) %>% 
-     rename(qc = WS_ms_Min_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 ws_max <- z %>% 
      filter(Variable=="WS_ms_Max") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WS_ms_Max_qc) %>% 
-     rename(qc = WS_ms_Max_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 ws_std <- z %>% 
      filter(Variable=="WS_ms_Std") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WS_ms_Std_qc) %>% 
-     rename(qc = WS_ms_Std_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 wdir <- z %>% 
      filter(Variable=="WindDir_D1_WVT") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WindDir_D1_WVT_qc) %>% 
-     rename(qc = WindDir_D1_WVT_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 wdir_sd <- z %>% 
      filter(Variable=="WindDir_SD1_WVT") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,WindDir_SD1_WVT_qc) %>% 
-     rename(qc = WindDir_SD1_WVT_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Wind")
 rain <- z %>% 
      filter(Variable=="Rain_mm_Tot") %>% 
-     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,Rain_mm_Tot_qc) %>% 
-     rename(qc = Rain_mm_Tot_qc) %>% 
+     select(unix_utc,Value,time_et,utc_offset,time_utc,site,Variable,source,qc) %>% 
      mutate(method = "Rain")
-export <- rbind(batt,air_avg,air_min,air_max,air_std,rh_min,rh_max,ws_avg,ws_min,ws_max,ws_std,wdir,wdir_sd,rain)
+level1 <- rbind(batt,air_avg,air_min,air_max,air_std,rh_min,rh_max,ws_avg,ws_min,ws_max,ws_std,wdir,wdir_sd,rain)
 rm(batt,air_avg,air_min,air_max,air_std,rh_min,rh_max,ws_avg,ws_min,ws_max,ws_std,wdir,wdir_sd,rain)
 
-export <- export[order(export$unix_utc),]
-export <- export[,c(2,3,4,5,6,7,10,8,9)]
-export <- export %>% 
+level1 <- level1[order(level1$unix_utc),]
+level1 <- level1[,c(2,3,4,5,6,7,10,8,9)]
+level1 <- level1 %>% 
      mutate(time_utc = as.character(time_utc)) %>% 
-     mutate(time_et = as.character(time_et))
+     mutate(time_et = as.character(time_et)) # Should now have 9 columns/variables
 
-write_csv(export, "/Users/davidkahler/Documents/Wind_Turbines/DuqMellon_HIS.csv", append = TRUE)
+write_csv(level1, "/Users/davidkahler/Documents/Wind_Turbines/DuqMellon_HIS.csv", append = TRUE)
 
 today <- Sys.Date()
 last_record <- x$unix_utc[nrow(x)]
@@ -403,7 +392,10 @@ write_csv(r, "download_record.csv", append = TRUE)
 
 # For export to hydro-lab.github.io, daily data and plots
 this <- week(Sys.Date())
-lastweek <- export %>% 
+lastweek <- cr1000x %>% 
+     pivot_longer(cols = c(BattV_Avg,AirTC_Avg,AirTC_Min,AirTC_Max,AirTC_Std,RHpct_Min,RHpct_Max,WS_ms_Avg,WS_ms_Min,WS_ms_Max,WS_ms_Std,WindDir_D1_WVT,WindDir_SD1_WVT,Rain_mm_Tot),
+                       names_to = "Variable",
+                       values_to = "Value") %>% 
      mutate(time_et=ymd_hms(time_et)) %>% 
      mutate(w = week(time_et)) %>% 
      filter(w == (this-1)) %>% 
@@ -443,7 +435,7 @@ wind <- lastweek %>%
      select(time_et,WS_ms_Min,WS_ms_Avg,WS_ms_Max,WindDir_D1_WVT) %>% 
      rename(spd=WS_ms_Avg,dir=WindDir_D1_WVT)
 
-m <- max(wind$WS_ms_Avg)
+m <- max(wind$spd)
 
 speed.bins <- 6
 dir.bins <- 36
