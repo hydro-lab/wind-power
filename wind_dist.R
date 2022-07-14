741850d626b68bdef8ead46f2bd93ea1c6e2d20e
mkranii <85361544+mkranii@users.noreply.github.com>
2022-07-14 11:00:02 -0400

    test

--- wind_dist.R
+++ wind_dist.R
@@ -1,8 +1,13 @@
-library(e1071)
+library(readr)
+library(dplyr)
+library(lubridate)
 
-x <- rnorm(1000)
-hist(x)
-mean(x)
-sd(x)
-kurtosis(x)
-skewness(x)
+#Collected 24 May 2022
+may.wind <- read.csv("https://duq.box.com/shared/static/hmths6ofu40ox2n9shvrdydawd57e7v2.dat")
+
+may.wind <- rnorm(1000)
+hist(may.wind)
+mean(may.wind)
+sd(may.wind)
+kurtosis(may.wind)
+skewness(may.wind)
