c212f76945bf6dcd0f7c7d722094f97a5def34af
David Kahler <david.m.kahler@gmail.com>
2023-04-05 21:56:51 -0400

    reindented lines 34-56

--- windspeed-mean-sds.R
+++ windspeed-mean-sds.R
@@ -31,29 +31,29 @@ x1 <- x1 %>%
 #x <- rbind(x2,x1)
 
 #renaming columns then removing things we do not need (RECORD, -WS_ms_S_WVT, -AirTC_TMx, -AirTC_TMn, -WS_ms_TMx, -WS_ms_TMn, -RHpct_TMx, -RHpct_TMn) then changing to correct time format for us and Cuahsi
-rename(TIMESTAMP = X1, # date and time
-       RECORD = X2, # sequential record number
-       BattV_Avg = X3, # battery voltage, average of 15 minute
-       AirTC_Avg = X4, # air temperature, degrees C
-       AirTC_Max = X5, # maximum temperature over period
-       AirTC_TMx = X6, # time of maximum temperature
-       AirTC_Min = X7, # minimum temperature over period
-       AirTC_TMn = X8, # time of minimum temperature
-       AirTC_Std = X9, # standard deviation of temperature over period: 1 Hz over 15 minutes
-       WS_ms_Avg = X10, # wind speed in m/s
-       WS_ms_Max = X11, # maximum wind speed over period
-       WS_ms_TMx = X12, # time of maximum wind speed
-       WS_ms_Min = X13, # minimum wind speed
-       WS_ms_TMn = X14, # time of minimum wind speed
-       WS_ms_Std = X15, # standard deviation of wind speed over period
-       WS_ms_S_WVT = X16, # don't know; appears to be same as average
-       WindDir_D1_WVT = X17, # wind direction
-       WindDir_SD1_WVT = X18, # standard deviation of wind direction
-       Rain_mm_Tot = X19, # incremental rainfall, mm
-       RHpct_Max = X20, # maximum relative humidity
-       RHpct_TMx = X21, # time of maximum relative humidity
-       RHpct_Min = X22, # minimum relative humidity
-       RHpct_TMn = X23) %>% # time of minimum relative humidity
+     rename(TIMESTAMP = X1, # date and time
+            RECORD = X2, # sequential record number
+            BattV_Avg = X3, # battery voltage, average of 15 minute
+            AirTC_Avg = X4, # air temperature, degrees C
+            AirTC_Max = X5, # maximum temperature over period
+            AirTC_TMx = X6, # time of maximum temperature
+            AirTC_Min = X7, # minimum temperature over period
+            AirTC_TMn = X8, # time of minimum temperature
+            AirTC_Std = X9, # standard deviation of temperature over period: 1 Hz over 15 minutes
+            WS_ms_Avg = X10, # wind speed in m/s
+            WS_ms_Max = X11, # maximum wind speed over period
+            WS_ms_TMx = X12, # time of maximum wind speed
+            WS_ms_Min = X13, # minimum wind speed
+            WS_ms_TMn = X14, # time of minimum wind speed
+            WS_ms_Std = X15, # standard deviation of wind speed over period
+            WS_ms_S_WVT = X16, # don't know; appears to be same as average
+            WindDir_D1_WVT = X17, # wind direction
+            WindDir_SD1_WVT = X18, # standard deviation of wind direction
+            Rain_mm_Tot = X19, # incremental rainfall, mm
+            RHpct_Max = X20, # maximum relative humidity
+            RHpct_TMx = X21, # time of maximum relative humidity
+            RHpct_Min = X22, # minimum relative humidity
+            RHpct_TMn = X23) %>% # time of minimum relative humidity
      select(-AirTC_TMx, -AirTC_TMn, -WS_ms_TMx, -WS_ms_TMn, -RHpct_TMx, -RHpct_TMn) %>%
      pivot_longer(c(BattV_Avg, AirTC_Avg, AirTC_Max, AirTC_Min, AirTC_Std, WS_ms_Avg, WS_ms_Max, WS_ms_Min, WS_ms_Std, WS_ms_S_WVT, WindDir_D1_WVT, WindDir_SD1_WVT, Rain_mm_Tot, RHpct_Max, RHpct_Min), names_to="measurement",values_to="values")
 
